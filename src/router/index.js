f4bf92014a071eb59d94a80fc66534df9eeea91d
Jerryemmanuel01 <jerryemmanuelolisa@gmail.com>
2023-12-18 15:44:13 +0100

    jobsDetails.vue added

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import About from "../views/AboutView.vue"
+import Jobs from "../views/jobs/Jobs.vue"
+import JobDetails from "../views/jobs/JobDetails.vue"
 
 const routes = [
   {
@@ -10,11 +13,19 @@ const routes = [
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    component: About
+  },
+  {
+    path: '/jobs',
+    name: 'jobs',
+    component: Jobs
+  },
+  {
+    path: '/jobs/:id',
+    name: 'jobdetails',
+    component: JobDetails,
+    props:true
+  },
 ]
 
 const router = createRouter({
